f80991d0ddea4c4182a490f140077a8fd77847a1
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-05-28 14:27:35 -0300

    Adiciona exemplos para validação do cartão.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,38 @@
 class PaymentsController < ApplicationController
   def create
-    credit_card = CreditCard.new(number: payment_params[:number])
-    if credit_card.valid?
-      # create payment
-      @payment = OpenStruct.new(value: 4.2, created_at: Time.now.to_date.to_s, status: :created)
+    credit_card = CreditCard.new(card_params)
+    @payment = Payment.new(payment_params)
+
+    if credit_card.valid? && @payment.valid?
+      slowify!
+      @payment.accept!
     else
-      render json: {nope: false}, status: :unprocessable_entity
+      render json: {
+        errors: [
+          {payment: @payment.errors},
+          {card: credit_card.errors}
+        ]
+      }, status: :unprocessable_entity
     end
   end
 
   private
 
+  def slowify!
+    sleep(rand(0..4))
+  end
+
+  def new_payment_params
+    params.require(:payment)
+  end
+
+  def card_params
+    # params.permit(:number, :valid, :cvv)
+    new_payment_params.permit(:number, :valid, :cvv)
+  end
+
   def payment_params
-    params.require(:payment).permit(:value, :number, :valid, :cvv)
+    # params.permit(:value)
+    new_payment_params.permit(:value)
   end
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,6 +1,28 @@
 class CreditCard
   include ActiveModel::Model
 
-  attr_accessor :number
+  attr_accessor :number, :valid, :cvv
   validates :number, credit_card_number: true
+  validate :future?
+
+  private
+
+  def valid_date?
+    begin
+      Date.parse(self.valid)
+      true
+    rescue Date::Error
+      false
+    end
+  end
+
+  def future?
+    if valid_date?
+      if Date.parse(self.valid) < Date.today
+        errors.add(:valid, "#{self.valid} is not a future date")
+      end
+    else
+      errors.add(:valid, "#{self.valid} is an invalid date")
+    end
+  end
 end
